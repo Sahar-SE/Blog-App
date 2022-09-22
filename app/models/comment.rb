5518d0c9a2b5f5e16a6f5a2f18dfccf5073b5397
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-09-23 01:14:54 +0430

    Add counter method on comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_save :update_comment_counter
+
+  def update_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
