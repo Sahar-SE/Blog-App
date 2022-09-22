5466f782e874847fd025f50d4d96f5e18e104efe
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-09-22 17:13:02 +0430

    Add post model and its columns

---
+++ db/migrate/20220922124126_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.integer :authorId
+      t.string :title
+      t.string :text
+      t.integer :commentsCounter
+      t.integer :likesCounter
+
+      t.timestamps
+    end
+  end
+end
