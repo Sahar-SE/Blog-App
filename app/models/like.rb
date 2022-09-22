8b650a8133df82655ca78023f30941598d80f941
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-09-23 01:15:29 +0430

    Add counter method on likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_save :update_like_counter
+
+  def update_like_counter
+    post.increment!(:likes_counter)
+  end
 end
