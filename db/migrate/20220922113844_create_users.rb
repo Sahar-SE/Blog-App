8bbad597cfbd6f60c5d542056380f3fde2da1d3d
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-09-22 22:50:27 +0430

    Add association to user migrate

--- db/migrate/20220922113844_create_users.rb
+++ db/migrate/20220922113844_create_users.rb
@@ -5,6 +5,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :photo
       t.text :bio
       t.integer :posts_counter
+      t.has_many :post
+      t.has_many :comment
+      t.has_many :like
 
       t.timestamps
     end
