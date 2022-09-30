2063d83e4c0d35ade6bcb1a23638a167c7d7dd1a
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-09-30 14:25:29 +0430

    Update user and post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
-class PostController < ApplicationController
-  def show; end
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:authorId])
+    @posts = @user.posts
+  end
 
-  def index; end
+  def show
+    @user = User.find(params[:authorId])
+    @post = @user.posts.find(params[:id])
+  end
 end
--- app/controllers/user_controller.rb
+++
@@ -1,5 +0,0 @@
-class UserController < ApplicationController
-  def show; end
-
-  def index; end
-end
