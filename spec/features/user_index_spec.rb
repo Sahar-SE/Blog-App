036bbc98e45613f84ae93d1c2e8ac746de513ef7
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-10-06 14:11:17 +0430

    Add text for user_index file

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'user_index', type: :feature do
+  before(:each) do
+    visit root_path
+  end
+
+  it 'shows the username of the users' do
+    expect(page).to have_content('Tom')
+  end
+
+  it 'shows the photos of the users' do
+    expect(page.html).to include('user-photo')
+  end
+
+  it 'shows the number of post of each user' do
+    user = User.last
+    expect(page).to have_content("Number of posts: #{user.postsCounter}")
+  end
+
+end
